baee7b47c7ac2c1628c11f07daed2294e90ee777
Rodrigo Menacho <rmenacho21@gmail.com>
2019-01-31 10:03:58 -0400

    Pivotal

--- definitions.py
+++ definitions.py
@@ -1,4 +1,4 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-SCHEMA_CREATION = os.path.join(ROOT_DIR, 'projectcreation.schema.json')
+SCHEMA_CREATION = os.path.join(ROOT_DIR, 'projectcreation.schema.todoly.json')
